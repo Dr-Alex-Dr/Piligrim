46576b8805de1cc71dfc1d796c3b3ae58272cd8b
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-12-16 19:14:12 +0300

    Updates

---
+++ precache-manifest.6f46f71dcbedca7ad01558ef5471378b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57dbb9ed798d36a956053398720c944",
+    "url": "/Piligrim/index.html"
+  },
+  {
+    "revision": "11d18c8f4b57761d0e27",
+    "url": "/Piligrim/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6ad778e727f5db88280d",
+    "url": "/Piligrim/static/css/main.93cd546f.chunk.css"
+  },
+  {
+    "revision": "11d18c8f4b57761d0e27",
+    "url": "/Piligrim/static/js/2.54d28aac.chunk.js"
+  },
+  {
+    "revision": "6ad778e727f5db88280d",
+    "url": "/Piligrim/static/js/main.aa29a554.chunk.js"
+  },
+  {
+    "revision": "892e5b603c65e0c18c57",
+    "url": "/Piligrim/static/js/runtime~main.bc0d4c62.js"
+  }
+]);
