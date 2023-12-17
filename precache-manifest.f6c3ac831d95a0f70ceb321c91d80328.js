ad923399b3483e7a0e6f8168ccd85f8c3f85241f
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-12-17 21:35:34 +0300

    Updates

--- precache-manifest.f6c3ac831d95a0f70ceb321c91d80328.js
+++ precache-manifest.f6c3ac831d95a0f70ceb321c91d80328.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a66f96a500c406630b3c5b0942e3b93",
+    "revision": "0ad0b5892ba186de940381a0e6093c4f",
     "url": "/Piligrim/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Piligrim/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "8b95c1d90ad2066263be",
-    "url": "/Piligrim/static/css/main.30955261.chunk.css"
+    "revision": "8b76007408639b12a5fd",
+    "url": "/Piligrim/static/css/main.57bbbcee.chunk.css"
   },
   {
     "revision": "bb625769cbfb260bcd06",
     "url": "/Piligrim/static/js/2.fd7db5e6.chunk.js"
   },
   {
-    "revision": "8b95c1d90ad2066263be",
+    "revision": "8b76007408639b12a5fd",
     "url": "/Piligrim/static/js/main.549c22e1.chunk.js"
   },
   {
